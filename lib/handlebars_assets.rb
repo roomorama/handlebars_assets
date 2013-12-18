514baef1a66d3d90a1a8910dafa783f71c99fb25
Donald Piret <donald@donaldpiret.com>
2013-12-18 22:17:34 +0800

    try another initializer

--- lib/handlebars_assets.rb
+++ lib/handlebars_assets.rb
@@ -1,4 +1,11 @@
 require "handlebars_assets/version"
+require 'rails'
+require 'sprockets'
+begin
+  require 'sprockets/railtie'
+rescue LoadError
+  require 'sprockets/rails/railtie'
+end
 
 module HandlebarsAssets
   PATH = File.expand_path("../../vendor/assets/javascripts", __FILE__)
@@ -11,13 +18,5 @@ module HandlebarsAssets
   autoload(:Handlebars, 'handlebars_assets/handlebars')
   autoload(:TiltHandlebars, 'handlebars_assets/tilt_handlebars')
 
-  if defined?(Rails) && defined?(::Rails::Engine)
-    require 'handlebars_assets/engine'
-  else
-    require 'sprockets'
-    Sprockets.register_engine '.hbs', TiltHandlebars
-    Sprockets.register_engine '.handlebars', TiltHandlebars
-    Sprockets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
-    Sprockets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
-  end
+  require 'handlebars_assets/railtie'
 end
---
+++ lib/handlebars_assets/railtie.rb
@@ -0,0 +1,13 @@
+module HandlebarsAssets
+  class Railtie < ::Rails::Railtie
+    config.before_initialize do |app|
+      require 'handlebars'
+      require 'handlebars_assets'
+      Sprockets::Engines
+      Sprockets.register_engine '.hbs', TiltHandlebars
+      Sprockets.register_engine '.handlebars', TiltHandlebars
+      Sprockets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
+      Sprockets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
+    end
+  end
+end
