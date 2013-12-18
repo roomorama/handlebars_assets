4d2f5217772e9aa71d43ddbb837a546137dad07f
Donald Piret <donald@donaldpiret.com>
2013-12-18 23:05:46 +0800

    last try

--- lib/handlebars_assets.rb
+++ lib/handlebars_assets.rb
@@ -18,5 +18,15 @@ module HandlebarsAssets
   autoload(:Handlebars, 'handlebars_assets/handlebars')
   autoload(:TiltHandlebars, 'handlebars_assets/tilt_handlebars')
 
+  if defined?(Rails) && defined?(::Rails::Engine)
+    require 'handlebars_assets/engine'
+  else
+    require 'sprockets'
+    Sprockets.register_engine '.hbs', TiltHandlebars
+    Sprockets.register_engine '.handlebars', TiltHandlebars
+    Sprockets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
+    Sprockets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
+  end
+
   require 'handlebars_assets/railtie'
 end
--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -1,11 +1,4 @@
 module HandlebarsAssets
   class Engine < ::Rails::Engine
-    initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :all do |app|
-      next unless Sprockets
-      Sprockets.register_engine('.hbs', TiltHandlebars)
-      Sprockets.register_engine('.handlebars', TiltHandlebars)
-      Sprockets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
-      Sprockets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
-    end
   end
 end
